5430a411d0e38504da9cb322beb83ac72c40611c
Ana <atripunoska@gmail.com>
2020-10-24 18:30:34 +0200

    Added responsive adjustments

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,13 @@ import React from "react";
 import Aux from "../../hoc/Auxiliary";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import classes from "./Layout.module.css";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const layout = (props) => {
   return (
     <Aux>
       <div>
-        <Toolbar />, SideDrawer, Backdrop
+        <Toolbar />, <SideDrawer />, Backdrop
       </div>
       <main className={classes.Content}>{props.children}</main>
     </Aux>
