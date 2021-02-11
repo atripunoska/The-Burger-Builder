ec9b35e7b53325c01b115515ff5ffb613d5787cf
Ana <atripunoska@gmail.com>
2021-02-11 21:40:13 +0100

    Dynamically Create Inputs based on JS Config and add a Dropdown Component

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -7,17 +7,40 @@ const input = (props) => {
   switch (props.elementType) {
     case "input":
       inputElement = (
-        <input className={classes.InputElement} {...props.elementConfig} value = {...props.value}/>
+        <input
+          className={classes.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+        />
       );
       break;
     case "textarea":
       inputElement = (
-        <textarea className={classes.InputElement} {...props.elementConfig} value = {...props.value} />
+        <textarea
+          className={classes.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+        />
+      );
+      break;
+    case "select":
+      inputElement = (
+        <select className={classes.InputElement} value={props.value}>
+          {props.elementConfig.options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.displayValue}
+            </option>
+          ))}
+        </select>
       );
       break;
     default:
       inputElement = (
-        <input className={classes.InputElement} {...props.elementConfig} value = {...props.value} />
+        <input
+          className={classes.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+        />
       );
   }
 
